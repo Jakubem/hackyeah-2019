0c9a03a8cde83b94ae35cda83bfe8c6ff52582d9
Jakubem <jakubmx@gmail.com>
2019-09-15 01:36:17 +0200

    Add stuff

--- src/js/main.js
+++ src/js/main.js
@@ -7,19 +7,21 @@ const config = {};
 window.config = config;
 
 const render = () => {
-    const quantity = document.querySelector('#meat-quantity').value || 1;
-    const interval = document.querySelector('#interval').value || 1;
+  const quantity = document.querySelector('#meat-quantity').value || 1;
+  const interval = document.querySelector('#interval').value || 1;
 
-    const amount = quantity / 1000;
-    const meetTypeMultiplier = data.find(product => product.name === config.type).waterPerKg;
-    const value = Number(Number.parseFloat(`${meetTypeMultiplier * amount * interval / 1000}`).toFixed(2)); // 1000 makes it liters
-    document.querySelector('.display').innerHTML = `${value}l`;
+  const amount = quantity / 1000;
+  const meetTypeMultiplier = data.find(product => product.name === config.type).waterPerKg;
+  const value = Number(Number.parseFloat(`${meetTypeMultiplier * amount * interval / 1000}`).toFixed(2)); // 1000 makes it liters
+  document.querySelector('.display').value = `${value}l`;
+  document.querySelector('.meat-quantity__output').value = quantity;
+  document.querySelector('.interval__output').value = interval;
 }
 
 Object.defineProperty(config, 'type', {
-    get() {
-        return document.querySelector('input[name="food-type"]:checked').value;
-    },
+  get() {
+    return document.querySelector('input[name="food-type"]:checked').value;
+  },
 });
 
 document.querySelectorAll('input').forEach(e => e.addEventListener('input', render));
