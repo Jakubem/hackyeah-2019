4eb5385cbeaac5fc38f90788106ede4bad1050ce
Wojciech Połowniak <wojciech.polowniak@gmail.com>
2019-09-14 17:23:48 +0200

    kodu kodu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
-console.log('hejka wojt');
+import * as Helpers from './utils/helpers';
+
+
+const button = document.querySelector('#check');
+button.addEventListener('click', (e) => {
+    const quantity = document.querySelector('#meat-quantity').value || 1;
+    const interval = document.querySelector('#interval').value || 1;
+
+    const amount = quantity / 1000;
+    const data = Helpers.getMetadata();
+    const beef = data.find(product => product.name === 'beef');
+
+    document.write(`${beef.waterPerKg * amount * interval}ml`);
+});
---
+++ src/js/utils/data.js
@@ -0,0 +1,6 @@
+const valueMap = [{
+    name: 'beef',
+    waterPerKg: 15400 * 1000,
+}];
+
+export default valueMap;
